b2d43abe64fc541a61ab166a31cb9cc3e366ea84
bsao17 <greta.b.meddhed>
2020-08-08 21:05:30 +0200

    try add marker on map

--- src/components/services/map.vue
+++ src/components/services/map.vue
@@ -19,10 +19,29 @@
 
         </section>
 
+        <!-- map-->
+
         <div id="map">
-            
-           
-            
+
+            <div >
+                <l-map
+                :zoom="zoom"
+                :center="center"
+                style="height: 500px; width: 100%"
+                >
+                <l-tile-layer
+                    :url="url"
+                    :attribution="attribution"
+                />
+                
+                </l-map>
+
+                <l-marker
+                :marker=markerMap
+                ></l-marker>
+
+            </div>
+                    
         </div>
         
 
@@ -39,33 +58,45 @@
 
 <script>
 
-
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 
 export default {
     data(){
         return{
             city: null,
             results: [],
-            index: 0
-            
+            index: 0,
+            zoom: 12,
+            center: latLng(43.6043, 1.4437),
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            markerLat: [],
+            markerLng: [],
+            markerMap: latLng(this.results)
         }
     },
     methods:{
         requestAjax:function(){
             const cityData = document.getElementById('city').value;
             this.city = cityData
-            console.log(this.city, this.results);
+            
             fetch(`https://api.jcdecaux.com/vls/v1/stations?contract=${cityData}&apiKey=e56f43cd9e4a4aa5260f59360a683fa28aaa4e6b`)
             .then(result => result.json())
             .then(response=>{
                 for(let i of response){
-                    this.results.push(i.position)
+                    this.results.push(i.position.lat, i.position.lng)
+                    this.markerLat.push(i.position.lat)
+                    this.markerLng.push(i.position.lng)
                 }
             });
+            console.log(this.results)
         }
     },
     components:{
-       
+        LMap,
+        LTileLayer,
+        LMarker
     }
 }
 </script>
