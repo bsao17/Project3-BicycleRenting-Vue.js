88fecc970a84cb84bb2ff2aea3dac32f7daa958a
bsao17 <greta.b.meddhed>
2020-08-10 09:13:01 +0200

    add multis marker Ok

--- src/components/services/map.vue
+++ src/components/services/map.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="container">
         <section class="container">
-
             <select class="card card-outline-primary" name="city" id="city">
                 <option value="toulouse">Toulouse</option>
                 <option value="lyon">Lyon</option>
@@ -16,37 +15,32 @@
             </select>
 
             <button class="btn btn-primary text-light" id="buttonChoice" @click="requestAjax">request</button>
-
         </section>
 
         <!-- map-->
-
         <div id="map">
 
             <div >
                 <l-map
                 :zoom="zoom"
                 :center="center"
-                style="height: 500px; width: 100%"
+                style="height: 600px; width: 100%"
                 >
                 <l-tile-layer
                     :url="url"
-                    :attribution="attribution"
                 />
                 <l-marker :lat-lng="markerLatLng" ></l-marker>
+                <l-marker
+                v-for="marker of markers"
+                :lat-lng="marker"
+                :key="marker"
+
+                ></l-marker>
                 
+
                 </l-map>
-
-
-            </div>
-                    
+            </div>      
         </div>
-        
-
-           
-
-
-
     </div>
 </template>
 
@@ -58,37 +52,37 @@
 
 // import { latLng } from "leaflet";
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import axios from 'axios'
 
 export default {
     data(){
         return{
             city: null,
-            results: [],
-            index: 0,
+            apiKey: 'e56f43cd9e4a4aa5260f59360a683fa28aaa4e6b',
+            reqData: [],
+            markers: [],
+            index:0,
             zoom: 12,
             center: [43.6043, 1.4437],
             url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-            markerLatLng: [43.6043, 1.4437],
-            markerLat: [],
-            markerLng: []
+            markerLatLng: [43.6, 1.45]
         }
     },
     methods:{
         requestAjax:function(){
             const cityData = document.getElementById('city').value;
             this.city = cityData
+            axios
+            .get(`https://api.jcdecaux.com/vls/v1/stations?contract=${cityData}&apiKey=${this.apiKey}`)
             
-            fetch(`https://api.jcdecaux.com/vls/v1/stations?contract=${cityData}&apiKey=e56f43cd9e4a4aa5260f59360a683fa28aaa4e6b`)
-            .then(result => result.json())
             .then(response=>{
-                for(let i of response){
-                    this.results.push(i.position.lat, i.position.lng)
-                    this.markerLat.push(i.position.lat)
-                    this.markerLng.push(i.position.lng)
+                this.reqData = response.data
+                console.log(this.reqData)
+                for(let e of this.reqData){
+                    this.markers.push(e.position)
                 }
+                console.log(this.marker)
             });
-            console.log(this.results)
         }
     },
     components:{
